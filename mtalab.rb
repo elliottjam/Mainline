b40cd7ba9ed9708fc62f9256436660c01045097e
James Elliott <elliottjam@gmail.com>
2013-06-19 18:36:08 +0100

    second push

--- mtalab.rb
+++ mtalab.rb
@@ -1,32 +1,31 @@
-which line do you want to get on?
-which station?
 
-whichl line do you want to get off?
-which station
+#& = interesection
 
+puts "----Get on the Mainline!----"
+puts "What line are you starting from (N, L, 6)?:"
+start_line = gets.chomp.downcase
+start_line.to_sym
+#if start_line == "6"
+  #then start_line = :s
+#end
+puts "At which station are you getting on?:"
+start_station = gets.chomp.downcase
+puts "Which line are you getting off on?:"
+end_line = gets.chomp.downcase.to_sym
+puts "Which station are you getting off at?:"
+end_station = gets.chomp.downcase
 
-& = interesection
+start_point=start_line.rindex(start_station)
+end_point=end_line.rindex(end_station)
 
-{[N,"Times Square"],[N,"34th Street"]}
-
-
-
-The N line has the following stops:
-     Times Square, 34th, 28th, 23rd, Union Square, and 8th
-
-   - The L line has the following stops:
-     8th, 6th, Union Square, 3rd, and 1st
-
-   - The 6 line has the following stops:
-     Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
-
-
-
-first_line = gets.chomp.to_sym
+result = (start_point - end_point).abs
 
 lines = {
-  :n => ['ts', 'us'],
-  :l => [ ],
-  :s => [ ],
+  :n => ['times square', '34th','28th-n','23rd-n','union square','8th'],
+  :l => [ '8th','6th','union square','3rd','1st'],
+  :s => [ 'grand central','33rd','28th-s','23rd-s','union square','astor place'],
+}
+
+
 
 
