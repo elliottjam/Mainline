1b60e77891281dc348ee68bffe4e238c06e229a8
James Elliott <elliottjam@gmail.com>
2013-06-19 17:57:16 +0100

    second commit

---
+++ mtalab.rb
@@ -0,0 +1,32 @@
+which line do you want to get on?
+which station?
+
+whichl line do you want to get off?
+which station
+
+
+& = interesection
+
+{[N,"Times Square"],[N,"34th Street"]}
+
+
+
+The N line has the following stops:
+     Times Square, 34th, 28th, 23rd, Union Square, and 8th
+
+   - The L line has the following stops:
+     8th, 6th, Union Square, 3rd, and 1st
+
+   - The 6 line has the following stops:
+     Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
+
+
+
+first_line = gets.chomp.to_sym
+
+lines = {
+  :n => ['ts', 'us'],
+  :l => [ ],
+  :s => [ ],
+
+
