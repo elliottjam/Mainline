baec201a6c190c3da11021844d557b245d946c07
Andrea Kennedy <asrkennedy@gmail.com>
2013-06-19 21:16:55 +0100

    final train journey with exchanges

--- mtalab.rb
+++ mtalab.rb
@@ -1,5 +1,5 @@
 
-require "pry"
+
 LINES = {
   'n' => ['times square', '34th','28th-n','23rd-n','union square','8th'],
   'l' => [ '8th','6th','union square','3rd','1st'],
@@ -16,9 +16,7 @@ def get_end(end_line)
   LINES[end_line].rindex end_station
 end
 
-def line_switch
-  puts "Which line are you getting off of (N, L, 6)?"
-    end_line = gets.chomp.downcase.to_s
+def line_switch(end_line)
     if end_line == "6"
       then end_line = 's'
       puts "Are you getting off at  Grand Central, 33rd, 28th-s, 23rd-s, Union Square, or Astor Place?"
@@ -26,7 +24,7 @@ def line_switch
     elsif end_line == 'n'
       puts "Are you getting off at Times Square, 34th, 28th-n, 23rd-n, Union Square, or 8th?"
       ending_point = get_end end_line
-    elsif end_line == 't'
+    elsif end_line == 'l'
       puts "Are you getting off at 8th, 6th, Union Square, 3rd, or 1st?'"
       ending_point = get_end end_line
     end
@@ -40,21 +38,47 @@ if start_line == "6"
     then start_line = 's'
     puts "Are you starting at  Grand Central, 33rd, 28th-s, 23rd-s, Union Square, or Astor Place?"
     starting_point = get_start start_line
-    ending_point = line_switch
+      puts "Which line are you getting off of (N, L, 6)?"
+      end_line = gets.chomp.downcase.to_s
+    ending_point = line_switch end_line
 elsif start_line == 'n'
     puts "Are you starting at Times Square, 34th, 28th-n, 23rd-n, Union Square, or 8th?"
     starting_point = get_start start_line
-    ending_point = line_switch
+        puts "Which line are you getting off of (N, L, 6)?"
+        end_line = gets.chomp.downcase.to_s
+    ending_point = line_switch end_line
 elsif start_line == "l"
     puts "Are you starting at 8th, 6th, Union Square, 3rd, or 1st?'"
     starting_point = get_start start_line
-    ending_point = line_switch
+        puts "Which line are you getting off of (N, L, 6)?"
+        end_line = gets.chomp.downcase.to_s
+    ending_point = line_switch end_line
+
 end
 
-if starting_point  and ending_point
+if start_line == end_line
   print (starting_point - ending_point).abs
-else
-  print "something wrong happened"
+end
+
+if start_line != end_line
+    if start_line == 'n' || 's'
+      union_square = 4
+      first_journey = (starting_point - union_square).abs
+      if end_line == 'l'
+        union_square = 2
+      elsif end_line == 's' || 'n'
+        union_square = 4
+      end
+      second_journey = (ending_point - union_square).abs
+      final_journey = first_journey + second_journey
+    elsif start_line == 'l'
+      union_square = 2
+      first_journey = (starting_point - union_square).abs
+      union_square = 4
+      second_journey = (ending_point - union_square).abs
+      final_journey = first_journey + second_journey
+    end
+puts "Your journey will take #{final_journey} stops."
 end
 
 
