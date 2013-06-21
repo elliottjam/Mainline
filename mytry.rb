9e79bb98e65d61df995028ddffdfb25927a205a8
James Elliott <elliottjam@gmail.com>
2013-06-21 14:19:32 +0100

    experiment

---
+++ mytry.rb
@@ -0,0 +1,7 @@
+
+require "pry"
+LINES = {
+  'n' => ['times square', '34th','28th-n','23rd-n','union square','8th'],
+  'l' => [ '8th','6th','union square','3rd','1st'],
+  's' => [ 'grand central','33rd','28th-s','23rd-s','union square','astor place'],
+}
